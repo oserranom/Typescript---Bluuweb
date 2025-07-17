05b7bebe523a3aa2cbe93e52f5b33a26574e4cae
Oscar <oscar.serrano91@gmail.com>
2025-07-17 21:21:43 +0200

    fruitlist to objects array

--- src/fruitlist.ts
+++ src/fruitlist.ts
@@ -2,12 +2,27 @@ import './style.css';
 
 
 //Array de primitivos no es necesario ser tipado, en caso contrario sí
-const fruitList: string[] = ["apple", "banana", "orange", "grape", "kiwi"]; 
+//const fruitList: string[] = ["apple", "banana", "orange", "grape", "kiwi"]; 
+
+//Para tipar un array de primitivos usamos "string[]", para objetos usamos "Array<type>" o "type[]"
+
+interface Fruit {
+    name: string;
+    color: string;
+}
+
+const fruitList: Fruit[] = [
+    { name: "apple", color: "red" },
+    { name: "banana", color: "yellow" },
+    { name: "orange", color: "orange" },
+    { name: "grape", color: "purple" },
+    { name: "kiwi", color: "green" }
+];
 
 const ulFruitList = document.querySelector("#fruitlist") as HTMLUListElement;
 
 fruitList.forEach((fruit) =>{
     const liFruit = document.createElement("li");
-    liFruit.textContent = fruit;
+    liFruit.textContent = `${fruit.name} - ${fruit.color}`;
     ulFruitList.appendChild(liFruit);
 }); 
