0ac281bdda019a8b768c18d1c202e7424b63a034
Óscar <oscar.serrano91@gmail.com>
2025-07-23 13:24:02 +0200

    interface Product and API call

--- 02- Ecommerce/src/product/product-list.ts
+++ 02- Ecommerce/src/product/product-list.ts
@@ -1,3 +1,12 @@
+import type { Product } from "../interfaces/product.interface";
+
 // 1. Capturar elementos desde la API 
+
+const getProducts = async (): Promise<Product[]> =>{
+    const response = await fetch("https://fakestoreapi.com/products");
+    const products = await response.json() as Product[]; 
+    return products; 
+}
+
 // 2. Renderizar la lista de productos en el DOM
 // 2.1 Crear la tarjeta card con el template html
