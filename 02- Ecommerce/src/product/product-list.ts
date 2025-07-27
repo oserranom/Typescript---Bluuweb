3462017547ddabaaa9068beffa6ced6eac19e5e2
Óscar <oscar.serrano91@gmail.com>
2025-07-27 18:19:10 +0200

    renderizando templates

--- 02- Ecommerce/src/product/product-list.ts
+++ 02- Ecommerce/src/product/product-list.ts
@@ -9,4 +9,20 @@ const getProducts = async (): Promise<Product[]> =>{
 }
 
 // 2. Renderizar la lista de productos en el DOM
+
+const renderProductList = async () =>{
+    const products = await getProducts(); 
+    const productList = document.querySelector("#product-list") as HTMLDivElement;
+    const productTemplate = document.querySelector("#product-template") as HTMLTemplateElement;
+
+    products.forEach(({title, id, image, price})=>{
+        //Ojo a esta sintáxis para templates
+        const product = productTemplate.content.cloneNode(true) as HTMLDivElement;
+        product.querySelector("img")!.src = image;
+        product.querySelector("h2")!.textContent = title;
+        product.querySelector("span")!.textContent = `${price} €`;
+        product.querySelector("a")!.href = `product-detail.html?id=${id}`;
+        productList.appendChild(product);
+    });
+}
 // 2.1 Crear la tarjeta card con el template html
