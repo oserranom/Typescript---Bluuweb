e49c77939b01172884cc87705c17399b283cecb6
Óscar <oscar.serrano91@gmail.com>
2025-08-02 16:22:21 +0200

    inicio lógica carrito cart.ts

---
+++ 02- Ecommerce/src/cart/cart.ts
@@ -0,0 +1,15 @@
+
+//1. Definir variable carrito CartItem[]
+
+
+
+//2. Crear un método para agregar item al carrito
+
+
+//3. Crear un método para eliminar del carrito
+
+
+//4. Crear un método para obtener el total del carrito
+
+
+//5. Crear un método para vaciar carrito
