a2c43cc17caeafa75a89279cb3128285fcdde613
Óscar <oscar.serrano91@gmail.com>
2025-08-09 18:10:39 +0200

    herencia y polimorfismo

--- 03 - Clases/src/main.ts
+++ 03 - Clases/src/main.ts
@@ -9,16 +9,53 @@ const pet = {
 
 //Clases
 //Inferencia directa no necesita tipar pero sí requiere valor por default 
-//Se puede evitar valor por default con el assertion operator (!)
+//Se puede evitar valor por default con el assertion operator (!) pero no recomendable 
+
+//Las propiedades pueden ser (ahora) public (por default), private, protected y readonly 
 class Pet {
-  name = ""; 
-  age = 0;
+
+  //Sintáxis breve (typescript) para constructores 
+  constructor(private name: string, private age: number){}
+
+
+  //Accede con los get no accede a la propiedad directamente 
+  public getMessage():string {
+    return `La mascota ${this.name} tiene ${this.age} años`; 
+  }
+  
 }
 
-const myPet = new Pet();
+const myPet = new Pet("Mochi", 5);
 
-myPet.name = "Mochi";
-myPet.age = 5;
+console.log("CLASES: ")
 
+console.log(myPet); 
+console.log(myPet.getMessage()); 
 
-console.log(); 
+//HERENCIA
+class Dog extends Pet{
+  constructor(
+    name: string,
+    age: number,
+    public breed: string
+  ){
+    super(name, age);
+  }
+
+  public bark(): void{
+    console.log("Woof! Woof!"); 
+  }
+
+  //POLIMORFISMO
+  public getMessage(): string {
+    return `${super.getMessage()} y es de raza ${this.breed}`
+  }
+}
+
+const myDog = new Dog("Cuqui", 2, "Chiwawa");
+
+console.log("HERENCIA: ");
+console.log(myDog);
+
+console.log("POLIMORFISMO: ");
+console.log(myDog.getMessage()); 
