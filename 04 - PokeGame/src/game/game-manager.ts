2c22911c4ca6d67dcee20042a570d40ed30a2886
Óscar <oscar.serrano91@gmail.com>
2025-08-15 18:32:59 +0200

    GameManager

--- 04 - PokeGame/src/game/game-manager.ts
+++ 04 - PokeGame/src/game/game-manager.ts
@@ -1,4 +1,5 @@
 import type { Pokemon } from "../models/pokemon.model";
+import { PokemonService } from "../services/pokemon.service";
 
 export enum GameState {
     LOADING = "loading",
@@ -8,7 +9,7 @@ export enum GameState {
 }
 
 export class GameManager {
-    private _currentPokemon: Pokemon | null;
+    private _currentPokemon: Pokemon | null = null;
     private _score: number = 0;
     private _gameState: GameState = GameState.LOADING; 
 
@@ -22,5 +23,49 @@ export class GameManager {
 
     get gameState(){
         return this._gameState; 
+    }
+
+    //Inizializar el juego con un pokemons aleartorio
+
+    public async startNewGame(){
+        this._gameState = GameState.LOADING;
+
+        try {
+            this._currentPokemon = await PokemonService.getRandomPokemon();
+            this._gameState = GameState.PLAYING;
+
+        } catch (error) {
+            console.log("Error PokeAPI");
+            this._gameState = GameState.WRONG; 
+            throw error; 
+        }
+    }
+
+    //Procesar la respuesta del cliente
+
+    public makeGuess(guess: string){
+        if(!this._currentPokemon || this._gameState !== GameState.PLAYING){
+            console.error("Error");
+            return; 
+        }
+
+        const isCorrect = this.currentPokemon?.isNameCorrect(guess);
+
+        if(isCorrect){
+            this._score++;
+            this._gameState = GameState.CORRECT;
+            this.celebrateCorrectAnswer();
+        }else{
+            this._gameState = GameState.WRONG;
+        }
+
+    }
+
+    private celebrateCorrectAnswer(){
+        console.log("YEAH"); 
+    }
+
+    public async nextPokemon(){
+        await this.startNewGame(); 
     }
 }
