a62b2b750481cb708778eff2ae3d279e8bc07e0b
Óscar <oscar.serrano91@gmail.com>
2025-08-29 17:32:33 +0200

    fin pokeapp

--- 04 - PokeGame/src/main.ts
+++ 04 - PokeGame/src/main.ts
@@ -14,9 +14,15 @@ class App{
 
     public async init(){
         try {
-            
+            await this.gameManager.startNewGame();
+            this.gameUI.updateUI(); 
         } catch (error) {
-            
+            console.log(error);
         }
     }
 }
+
+document.addEventListener("DOMContentLoaded", async()=>{
+    const app = new App();
+    await app.init();
+}); 
