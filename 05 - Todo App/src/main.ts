45f432096ccd645190607920198b1df38df69654
Óscar <oscar.serrano91@gmail.com>
2025-08-31 20:29:26 +0200

    captura selectores, service.ts

--- 05 - Todo App/src/main.ts
+++ 05 - Todo App/src/main.ts
@@ -1,1 +1,11 @@
 import './style.css'
+
+document.addEventListener("DOMContentLoaded", ()=>{
+
+    //Selectores
+    const todoForm = document.querySelector("#todo-form") as HTMLFormElement;
+    const todoInput = document.querySelector("#todo-input") as HTMLInputElement; 
+    const todoList = document.querySelector("#todo-list") as HTMLUListElement;
+    
+    
+});
---
+++ 05 - Todo App/src/services/todo.servcice.ts
@@ -0,0 +1,10 @@
+
+interface Todo{
+    id: string;
+    description: string;
+    done: boolean; 
+}
+
+export const todoService = {
+
+}
