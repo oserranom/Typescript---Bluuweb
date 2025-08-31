763db4514ef7817df2b0626dc0727c4376152008
Óscar <oscar.serrano91@gmail.com>
2025-08-31 20:43:23 +0200

    loadTodos, saveTodos, getTodos

--- 05 - Todo App/src/services/todo.servcice.ts
+++ 05 - Todo App/src/services/todo.servcice.ts
@@ -1,8 +1,31 @@
 
-interface Todo{
+export interface Todo{
     id: string;
     description: string;
     done: boolean; 
+}
+
+let todos: Todo[] = []; 
+
+//Obtener lista de localStorage
+const loadTodos = () =>{
+    const todosJSON =  localStorage.getItem('todos');
+
+    if(!todosJSON){
+        todos = [];
+        return;
+    }
+
+    todos =  JSON.parse(todosJSON) as Todo[]; 
+}
+
+//Guardar lista en localStorage
+const saveTodos = (todos: Todo[]): void =>{
+    localStorage.setItem("todos", JSON.stringify(todos)); 
+}
+
+const getTodos = (): Todo[] =>{
+    return [...todos]; 
 }
 
 export const todoService = {
